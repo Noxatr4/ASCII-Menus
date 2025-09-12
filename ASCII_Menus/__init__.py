1ea449e5d781a0323ce818eb166e0e97f3f00b09
Martín <martinrore@yahoo.cl>
2025-09-12 16:35:01 -0300

    Complete-DocstringsComments

--- ASCII_Menus/__init__.py
+++ ASCII_Menus/__init__.py
@@ -61,7 +61,7 @@ class Menu:
         _cursor_coordinates : list[int, int, int]
                              It saves the cursor position.
         _option_per_column : int
-        _rows_per_page : int
+        _options_rows_per_page : int
         _character_per_option : int
         _number_pages : int
         _max_index : int
@@ -92,7 +92,7 @@ class Menu:
                            List with all the options in str format for the menu.
             dynamic_static_menu : bool, default=True
         """
-        def options_list_processing(input_options_list: list[str]):
+        def _options_list_processing(input_options_list: list[str]):
             """
             Process the ``input_options_list``, so that useful in class
 
@@ -145,8 +145,8 @@ class Menu:
 
             # Create Pages
             create_pages = []
-            for i in range(0, len(options_list_processed), self._rows_per_page):
-                create_pages.append(options_list_processed[i : i + self._rows_per_page])
+            for i in range(0, len(options_list_processed), self._options_rows_per_page):
+                create_pages.append(options_list_processed[i : i + self._options_rows_per_page])
 
             return create_pages
 
@@ -158,7 +158,7 @@ class Menu:
 
         # Menu structure
         self._option_per_column = option_per_column
-        self._rows_per_page = rows_per_page
+        self._options_rows_per_page = rows_per_page
         self._character_per_option = character_per_option
         self._number_pages = (int(len(options_list)
                                   / (option_per_column * rows_per_page))
@@ -170,17 +170,29 @@ class Menu:
                                     + 1)
         self._option_cutoff_point = (character_per_option
                                      - len(DEFAULT_CHARACTERS["CharacterOverflow"]))
-        self._options_list: list = options_list_processing(options_list)
+        self._options_list: list = _options_list_processing(options_list)
 
 
     def menu_crafter(self, mode: str = "solo"):
         """
+        Generate `Frame Menu`.
 
-        :param mode:
-        :return:
+        Parameters:
+            mode: str
+                  You want a return in list or None with print in terminal.
+        Return:
+            list | None
+                It depends on the value of ``mode``.
+
+        Examples:
+            - ``mode="solo"``: Return None; Print in terminal.
+            - ``mode="display"``: Return list with menu rows.
+
         """
 
-        def mode_menu_generate(input_row: str | tuple[str, ...]):
+        def _mode_menu_generate(input_row: str | tuple[str, ...]):
+            """It generate output dependent to you  select mode"""
+
             if mode == "solo":
                 print(input_row)
             elif mode == "display":
@@ -188,14 +200,24 @@ class Menu:
             else:
                 pass
 
-        def set_scrollbar():
-            scrollbar_groups = []
-            rows_group = int(self._rows_per_page * 2 / self._number_pages)
-            module = self._rows_per_page * 2 % self._number_pages
+        def _set_scrollbar():
+            """Create groups of rows that represent the pages for the scrollbar"""
 
+            scrollbar_groups = []
+            # How many options rows represents each page? (Total Rows / Total Page)
+            # Total Rows = "options rows" + "spaces row"
+            # As each type of row is interleaved, the result can be obtained
+            # Total Rows = "options rows" * 2
+            # rows_group is the size complete group
+            # module is the surplus
+            rows_group = int(self._options_rows_per_page * 2 / self._number_pages)
+            module = self._options_rows_per_page * 2 % self._number_pages
+
+            # Obtain the size of the rows groups by adding the surplus
             n_rows_groups = [rows_group + 1 if page < module else rows_group
                           for page in range(self._number_pages)]
 
+            # Assign the index of the rows that corresponds to each group
             ii = 0
             for page in range(self._number_pages):
                 scrollbar_groups.append([])
@@ -205,7 +227,9 @@ class Menu:
 
             return scrollbar_groups
 
-        def create_row(type_row: str, index_options_row: int = 0):
+        def _create_row(type_row: str, index_options_row: int = 0):
+            """Generate a standard row type"""
+
             match type_row:
                 case "limit":
                     return (DEFAULT_CHARACTERS["MenuCorner"]
@@ -221,15 +245,20 @@ class Menu:
                             +DEFAULT_CHARACTERS["RowLimit"])
                 case "options":
                     odd = index_options_row % 2
+
+                    # If you are in the row that corresponds to the active page of the scroll-bar
                     scrollbar_value = True if index_options_row in active_scrollbar else False
 
                     if odd:
+                        # Obtain the option row you need
                         row = int(  (index_options_row - 1) / 2  )
                         show_list_options = self._options_list[coord_page][row].copy()
 
+                        # Set the Scroll-Bar value
                         show_list_options[self._column_object_index(mode="ScrollBar")] = \
                             DEFAULT_CHARACTERS["ScrollBarValues"][scrollbar_value]
 
+                        # Set the cursor Value
                         if self.activated and self._type_menu and row == coord_row:
                             show_list_options[self._column_object_index(mode="cursor")] = \
                                 DEFAULT_CHARACTERS["cursor_values"][True]
@@ -243,7 +272,7 @@ class Menu:
                                 + DEFAULT_CHARACTERS["RowLimit"])
 
                 case _:
-                    return ""
+                    raise ValueError("Invalid type row")
 
 
         steps_to_create_menu = 5
@@ -251,29 +280,31 @@ class Menu:
         coord_page = self._cursor_coordinates[0]
         coord_row = self._cursor_coordinates[1]
 
+        # Generate menu parts
         for n in range(steps_to_create_menu):
             if n == 1:
-                title = create_row("title")
-                mode_menu_generate(title)
+                title = _create_row("title")
+                _mode_menu_generate(title)
             elif n == 2:
-                separator_row = create_row("separator")
-                mode_menu_generate(separator_row)
+                separator_row = _create_row("separator")
+                _mode_menu_generate(separator_row)
             elif n == 3:
-                active_scrollbar = set_scrollbar()[coord_page]
-                for c in range(self._rows_per_page * 2):
-                    options_row = create_row("options", index_options_row=c)
-                    mode_menu_generate(options_row)
+                active_scrollbar = _set_scrollbar()[coord_page]
+                for c in range(self._options_rows_per_page * 2):
+                    options_row = _create_row("options", index_options_row=c)
+                    _mode_menu_generate(options_row)
 
             else:
-                limit_row = create_row("limit")
-                mode_menu_generate(limit_row)
+                limit_row = _create_row("limit")
+                _mode_menu_generate(limit_row)
 
+        # Generate Returns
         if mode == "solo":
             return None
         elif mode == "display":
             return menu_list
         else:
-            return print("ERROR: En la función menu_crafter; Modo no válido")
+            raise ValueError("Invalid mode")
 
 
     def change_coordinate_cursor(self, input_user: str):
@@ -285,13 +316,14 @@ class Menu:
         """
 
         limit_coord = [self._number_pages,
-                       self._rows_per_page,
+                       self._options_rows_per_page,
                        self._option_per_column]
 
         coord_z = 0
         coord_y = 1
         coord_x = 2
 
+        # Obtain the coord value change
         if input_user.upper() == MOVE_UP:
             add_coord = (0, -1, 0)
         elif input_user.upper() == MOVE_DOWN:
@@ -303,10 +335,12 @@ class Menu:
         else:
             return
 
+        # sum the last coord
         self._cursor_coordinates = list(
             map(lambda x, y: x + y, self._cursor_coordinates, add_coord)
         )
 
+        # Refactor with the coord limit
         self._cursor_coordinates[coord_x] %= limit_coord[coord_x]
 
         if self._cursor_coordinates[coord_y] < 0 or self._cursor_coordinates[coord_y] >= limit_coord[coord_y]:
